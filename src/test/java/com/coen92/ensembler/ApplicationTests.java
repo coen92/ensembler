aeef2740ec3a26830cc98862f798774d94973a26
Piotr Kapica <piotrkapica92@gmail.com>
2024-02-01 23:15:23 +0100

    EN-1: application setup

---
+++ src/test/java/com/coen92/ensembler/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coen92.ensembler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
